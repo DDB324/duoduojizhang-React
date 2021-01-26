2330dd09ea67f0e17f7413b5b1fa32d8943432ed
ddb324 <jiangdaoran@163.com>
2021-01-26 13:40:18 +0800

    确定页面的URL

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,64 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div>Hello!</div>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/detail">明细</Link>
+          </li>
+          <li>
+            <Link to="/chart">图表</Link>
+          </li>
+          <li>
+            <Link to="/money">记账</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route path="/detail">
+            <Detail/>
+          </Route>
+          <Route path="/chart">
+            <Chart/>
+          </Route>
+          <Route path="/money">
+            <Money/>
+          </Route>
+          <Redirect exact from="/" to="/detail"/>
+          <Route path="*">
+            <NoMatch/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
+
+
+function Detail() {
+  return <h2>明细</h2>;
+}
+
+function Chart() {
+  return <h2>图表</h2>;
+}
+
+function Money() {
+  return <h2>记账</h2>;
+}
+
+function NoMatch() {
+  return <h2>页面不存在吖</h2>;
+}
+
 
 export default App;
