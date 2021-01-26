05a3d120abf488cfc8cc33edd7203ab995544731
ddb324 <jiangdaoran@163.com>
2021-01-26 14:42:43 +0800

    导航栏布局

--- src/App.tsx
+++ src/App.tsx
@@ -6,39 +6,66 @@ import {
   Link,
   Redirect,
 } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+`;
+
+const Main = styled.div`
+  flex-grow: 1;
+  overflow: auto;
+`;
+
+const Nav = styled.div`
+  > ul {
+    display: flex;
+
+    > li {
+      width: 33.3%;
+      text-align: center;
+      padding: 16px;
+    }
+  }
+`;
 
 function App() {
   return (
     <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to="/detail">明细</Link>
-          </li>
-          <li>
-            <Link to="/chart">图表</Link>
-          </li>
-          <li>
-            <Link to="/money">记账</Link>
-          </li>
-        </ul>
-
-        <Switch>
-          <Route path="/detail">
-            <Detail/>
-          </Route>
-          <Route path="/chart">
-            <Chart/>
-          </Route>
-          <Route path="/money">
-            <Money/>
-          </Route>
-          <Redirect exact from="/" to="/detail"/>
-          <Route path="*">
-            <NoMatch/>
-          </Route>
-        </Switch>
-      </div>
+      <Wrapper>
+        <Main>
+          <Switch>
+            <Route path="/detail">
+              <Detail/>
+            </Route>
+            <Route path="/chart">
+              <Chart/>
+            </Route>
+            <Route path="/money">
+              <Money/>
+            </Route>
+            <Redirect exact from="/" to="/detail"/>
+            <Route path="*">
+              <NoMatch/>
+            </Route>
+          </Switch>
+        </Main>
+        <Nav>
+          <ul>
+            <li>
+              <Link to="/detail">明细</Link>
+            </li>
+            <li>
+              <Link to="/chart">图表</Link>
+            </li>
+            <li>
+              <Link to="/money">记账</Link>
+            </li>
+          </ul>
+        </Nav>
+      </Wrapper>
     </Router>
   );
 }
