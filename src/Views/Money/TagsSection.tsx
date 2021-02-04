1c840195b9d7398dd2a693304cfd1a13e05a85b5
ddb324 <jiangdaoran@163.com>
2021-02-04 13:23:30 +0800

    点击新增标签的按钮,触发跳转

--- src/Views/Money/TagsSection.tsx
+++ src/Views/Money/TagsSection.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import Icon from '../../components/Icon';
-import React, {useState} from 'react';
+import React, {ReactHTMLElement, ReactNode, ReactNodeArray, useRef, useState} from 'react';
+import {Link} from 'react-router-dom';
 
 const Wrapper = styled.section`
   padding: 12px 20px 0;
@@ -51,12 +52,6 @@ const TagsSection: React.FC<Props> = (props) => {
     //这里只记录了名字,后面要做另一个页面展示logo,然后设置name,再把logo和name放到数组中展示出来
     //TODO
     const tags = props.tags;
-    // const addTag = () => {
-    //   const tagName = window.prompt('你要增加的标签的名字为?');
-    //   if (tagName !== null) {
-    //     setTags([...tags, tagName]);
-    //   }
-    // };
     const onToggleTag = (tag: string) => {
       const index = selectedTag?.indexOf(tag);
       if (index >= 0) {
@@ -66,6 +61,10 @@ const TagsSection: React.FC<Props> = (props) => {
       }
     };
     const getClass = (tag: string) => selectedTag?.indexOf(tag) >= 0 ? 'selected' : '';
+    const textLink = useRef(null);
+    const handleClick = () => {
+        (textLink.current! as HTMLElement).click();
+    };
     return (
       <Wrapper>
         <ul>
@@ -79,12 +78,13 @@ const TagsSection: React.FC<Props> = (props) => {
               </li>
             );
           })}
-          {/*<li onClick={addTag}>*/}
-          {/*  <div>*/}
-          {/*    <Icon name='money'/>*/}
-          {/*  </div>*/}
-          {/*  <span>新增标签</span>*/}
-          {/*</li>*/}
+          <li onClick={handleClick}>
+            <div>
+              <Icon name='money'/>
+            </div>
+            <span>新增标签</span>
+            <Link ref={textLink} to={'/tags/edit'}/>
+          </li>
         </ul>
       </Wrapper>
     );
