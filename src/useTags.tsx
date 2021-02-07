dbb3952fff98e693e0d9305fc644ddee09078da2
ddb324 <jiangdaoran@163.com>
2021-02-07 12:11:54 +0800

    自动生成tag的id

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -1,35 +1,35 @@
 import {useState} from 'react';
+import {createId} from './lib/createId';
 
 type Tag = {
-  id:number,
-  chart:string,
-  name:string
+  id: number,
+  chart: string,
+  name: string
 }
 
+const defaultIncomeTags = [
+  {id: createId(), chart: 'draw-money', name: '取钱'},
+  {id: createId(), chart: 'save-money', name: '存钱'},
+  {id: createId(), chart: 'wage', name: '工资'},
+];
+const defaultExpenditureTags = [
+  {id: createId(), chart: 'repast', name: '餐饮'},
+  {id: createId(), chart: 'shopping2', name: '购物'},
+  {id: createId(), chart: 'airplane', name: '旅行'},
+  {id: createId(), chart: 'amusement', name: '娱乐'},
+  {id: createId(), chart: 'cinema', name: '电影'},
+  {id: createId(), chart: 'game', name: '游戏'},
+  {id: createId(), chart: 'gift', name: '礼物'},
+  {id: createId(), chart: 'journey', name: '行李'},
+  {id: createId(), chart: 'maintain', name: '修理'},
+  {id: createId(), chart: 'music', name: '音乐'},
+  {id: createId(), chart: 'sing', name: '唱歌'},
+];
+
 const useTags = () => {
-  const [incomeTags, setIncomeTags] = useState<Tag[]>(
-    [
-      {id:1,chart: 'draw-money', name: '取钱'},
-      {id:2,chart: 'save-money', name: '存钱'},
-      {id:3,chart: 'wage', name: '工资'},
-    ]
-  );
-  const [expenditureTags, setExpenditureTags] = useState<Tag[]>(
-    [
-      {id:4,chart: 'repast', name: '餐饮'},
-      {id:5,chart: 'shopping2', name: '购物'},
-      {id:6,chart: 'airplane', name: '旅行'},
-      {id:7,chart: 'amusement', name: '娱乐'},
-      {id:8,chart: 'cinema', name: '电影'},
-      {id:9,chart: 'game', name: '游戏'},
-      {id:10,chart: 'gift', name: '礼物'},
-      {id:11,chart: 'journey', name: '行李'},
-      {id:12,chart: 'maintain', name: '修理'},
-      {id:13,chart: 'music', name: '音乐'},
-      {id:14,chart: 'sing', name: '唱歌'},
-    ]
-  );
+  const [incomeTags, setIncomeTags] = useState<Tag[]>(defaultIncomeTags);
+  const [expenditureTags, setExpenditureTags] = useState<Tag[]>(defaultExpenditureTags);
   // return {incomeTags: incomeTags ...}
-  return {incomeTags,setIncomeTags,expenditureTags,setExpenditureTags}
+  return {incomeTags, setIncomeTags, expenditureTags, setExpenditureTags};
 };
 export {useTags};
