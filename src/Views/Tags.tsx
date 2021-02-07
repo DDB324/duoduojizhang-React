7016efbdfc22b43a494ce50b939a31c5caceca5b
ddb324 <jiangdaoran@163.com>
2021-02-07 17:56:21 +0800

    增加删除tag的功能

--- src/Views/Tags.tsx
+++ src/Views/Tags.tsx
@@ -68,12 +68,38 @@ const Main = styled.div`
   }
 `;
 
+type Tag = {
+  id: number,
+  chart: string,
+  name: string
+}
+
 const Tags: React.FC = () => {
   //获取tag数据
-  const {expenditureTags, incomeTags} = useTags();
+  const {expenditureTags, incomeTags, setExpenditureTags, setIncomeTags} = useTags();
 
   //声明category变量
   const [category, setCategory] = useState<'-' | '+'>('-');
+
+  //删除tag
+  const removeTag = (tagId: number) => {
+    //声明'+''-'对应的操作
+    const categoryMap = {'-': expenditureTags, '+': incomeTags};
+    const setCategoryMap = {'-': setExpenditureTags, '+': setIncomeTags};
+
+    //深拷贝tags用来作为新的tags
+    const copyTags: Tag[] = JSON.parse(JSON.stringify(categoryMap[category]));
+
+    //找到要删除的tag的下标
+    const removedTag = copyTags.filter(item => item.id === tagId)[0];
+    const tagIndex = copyTags.indexOf(removedTag);
+
+    //增加确认删除
+    if (tagIndex >= 0 && window.confirm('确定要删除当前标签吗?')) {
+      copyTags.splice(tagIndex, 1);
+      setCategoryMap[category](copyTags);
+    }
+  };
   return (
     <Wrapper>
       <CategorySettings category={category}
@@ -88,7 +114,7 @@ const Tags: React.FC = () => {
                     <Icon name={tag.chart}/>
                   </div>
                   <span>{tag.name}</span>
-                  <button>删除</button>
+                  <button onClick={() => removeTag(tag.id)}>删除</button>
                 </li>
               );
             })
