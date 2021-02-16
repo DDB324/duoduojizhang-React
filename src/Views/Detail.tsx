4891e60c65bca16d41054e375d245d36e08111da
ddb324 <jiangdaoran@163.com>
2021-02-16 14:51:58 +0800

    引入并使用dayjs

--- src/Views/Detail.tsx
+++ src/Views/Detail.tsx
@@ -6,20 +6,21 @@ import {TopWrapper} from './Detail/TopWrapper';
 import {MainWrapper} from './Detail/MainWrapper';
 import {useRecords} from '../hooks/useRecords';
 import {useTags} from '../hooks/useTags';
+import day from 'dayjs';
 
 //显示记账明细的页面
 const Detail = () => {
   const {records} = useRecords();
   const {findTag} = useTags();
-  console.log(records);
+  const currentTime = new Date().toISOString();
   const top = () => {
     return <TopWrapper>
       <main><img src={title} alt=""/></main>
       <div>
         <div className='date'>
-          <div className='year'>2021年</div>
+          <div className='year'>{day(currentTime).format('YYYY年')}</div>
           <div className='month'>
-            <div className='month-content'>02</div>
+            <div className='month-content'>{day(currentTime).format('MM')}</div>
             <span>月</span>
             <span className='triangle'/>
             <span className='vertical-line'/>
@@ -37,37 +38,39 @@ const Detail = () => {
     </TopWrapper>;
   };
   const main = () => {
-    return <MainWrapper>
-      <div className='top'>
-        <div className='date'>02月15日 星期一</div>
-        <div className='income'>收入:&nbsp;&nbsp;10</div>
-        <div className='expenditure'>支出:&nbsp;&nbsp;5</div>
-      </div>
-      <ul>
-        {records.map(record => (
-          <li>
-            <div className='icon-wrapper'>
-              <Icon name={findTag(record.selectedTagId[0]).chart}/>
-            </div>
-            <div className='record-name'>
-              {findTag(record.selectedTagId[0]).name}
-              <span className='vertical-line'/>
-            </div>
-            <div className='record-amount'>{(record.category === '-' ? '-' : '+') + record.amount}</div>
-          </li>
-        ))}
-      </ul>
-    </MainWrapper>;
-  }
-;
-return (
-  <Layout
-    top={top()}
-    main={main()}
-  />
-);
+      return <MainWrapper>
+        <div className='top'>
+          <div className='date'>02月15日 星期一</div>
+          <div className='income'>收入:&nbsp;&nbsp;10</div>
+          <div className='expenditure'>支出:&nbsp;&nbsp;5</div>
+        </div>
+        <ul>
+          {records.map(record => (
+            <li>
+              <div className='icon-wrapper'>
+                <Icon name={findTag(record.selectedTagId[0]).chart}/>
+              </div>
+              <div className='record-name'>
+                {findTag(record.selectedTagId[0]).name + day(record.createAt).format('MM月DD日 dddd')}
+                <span className='vertical-line'/>
+              </div>
+              <div className='record-amount'>{(record.category === '-' ? '-' : '+') + record.amount}</div>
+            </li>
+          ))}
+        </ul>
+      </MainWrapper>;
+    }
+  ;
+  return (
+    <Layout
+      top={top()}
+      main={main()}
+    />
+  );
 };
 
 export
-  {Detail}
-;
+{
+  Detail
+}
+  ;
