0f681b4f66ccfca5d12870e7ddabbd594c84c792
ddb324 <jiangdaoran@163.com>
2021-02-22 02:00:38 +0800

    安装echarts并显示到页面中

--- src/Views/Chart.tsx
+++ src/Views/Chart.tsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import {Layout} from 'components/Layout';
 import Nav from '../components/Nav';
 import {Category, CategorySection} from './Money/CategorySection';
+import {ChartMain} from './Chart/ChartMain';
 
 //显示图表的页面
 const Chart = () => {
@@ -12,12 +13,7 @@ const Chart = () => {
         <CategorySection category={category}
                          onCategoryChange={(category) => {setCategory(category);}}/>
       }
-      main={
-        <div>
-          <div>Echarts</div>
-          <div>详细支出/收入内容</div>
-        </div>
-      }
+      main={<ChartMain/>}
       footer={Nav()}
     />
   );
---
+++ src/Views/Chart/ChartMain.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {ChartEcharts} from './ChartEcharts';
+
+const ChartMain:React.FC = ()=>{
+  return (
+    <div>
+      <ChartEcharts />
+    </div>
+  )
+}
+
+export {ChartMain}
